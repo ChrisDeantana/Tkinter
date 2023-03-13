d4628a0086cec0d8e1b414ff5946148dbff6beb7
Christian Deantana <74668737+ChrisDeantana@users.noreply.github.com>
2023-03-13 16:59:23 +0800

    Add files via upload

--- Section1_TkinterFundamentals/05_commandBinding.py
+++ Section1_TkinterFundamentals/05_commandBinding.py
@@ -16,4 +16,6 @@ ttk.Button(root, text='Click Me', command=button_clicked).pack()
 ttk.Button(root, text='Rock', command=lambda: select('Rock')).pack()
 ttk.Button(root, text='Paper', command=lambda: select('Paper')).pack()
 ttk.Button(root, text='Scissors', command=lambda: select('Scissors')).pack()
+
+
 root.mainloop()
